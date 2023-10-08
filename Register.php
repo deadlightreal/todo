24daf5a89bb9beec7c25be2ef4963d7942c45451
deadlightreal <richardfabianmain@gmail.com>
2023-10-08 10:09:02 +0200

    Commit 2

--- Register.php
+++ Register.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="CreateAccount.php" method="POST">
-        <input type="text" name="username" id="username">
-        <input type="email" name="email" id="email">
-        <input type="password" name="password" id="password">
-        <input type="submit" value="Register">
+    <form action="CreateAccount.php" class="main" method="POST">
+        <input type="text" name="username" id="username" placeholder="Enter username" required>
+        <input type="email" name="email" id="email" placeholder="Enter email" required>
+        <input type="password" name="password" id="password" placeholder="Enter password" required>
+        <input type="submit" value="Register" id="submit">
     </form>
 </body>
 </html>
