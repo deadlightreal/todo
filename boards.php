1e373d73a0d80ecbf99e6dabc911f7156c2966a9
deadlightreal <richardfabianmain@gmail.com>
2023-10-13 18:44:44 +0200

    Commit 19

---
+++ boards.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boards</title>
+</head>
+<body>
+    <form action="createBoard.php">
+        
+    </form>
+    <?php
+        session_start();
+        include "dbConnection.php";
+        $username = $_SESSION["username"];
+        $code = "SELECT * FROM `boards` WHERE (`owner`) LIKE ('$username') OR (`invited`) LIKE ('$username')";
+        $result = mysqli_query($sql, $code);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<div>" . $row["name"] . "</div>";
+                echo '<form action="board.php" method="POST>"';
+                echo '<input type="hidden" name="boardid" value="' . $row["id"] . '">';
+                echo '<input type="submit" value="Connect">';
+
+
+                echo '</form>';
+            }
+        }
+    ?>
+</body>
+</html>
